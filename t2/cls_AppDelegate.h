d55c8c652f1161123eb58b4e910b2ac512921edb
wagmob-sunil-gupta <Nitesh@Abhishek.local>
2014-06-07 17:22:05 +0530

    Initial Commit

---
+++ t2/cls_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  cls_AppDelegate.h
+//  t2
+//
+//  Created by Sunil on 6/7/14.
+//  Copyright (c) 2014 WAGmob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cls_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ t2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  t2
+//
+//  Created by Sunil on 6/7/14.
+//  Copyright (c) 2014 WAGmob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "cls_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([cls_AppDelegate class]));
+    }
+}
